195c550a3aa73df2c36a62fb001b99c2e5e7e63c
tobiasmuench <tobias.muench@campus.tu-berlin.de>
2018-01-13 17:11:22 +0100

    delivery to sarah for check

--- gplearn/complexity.py
+++ gplearn/complexity.py
@@ -3,7 +3,7 @@ import numpy as np
 def complexity(xdata, ydata):
     print("######### start complexity measurement ###############")
     print("xdata: \n",xdata)
-    print("ydata", ydata)
+    print("ydata: \n", ydata)
 
     #error checking
     nrowsx = xdata.shape[0]
@@ -17,76 +17,55 @@ def complexity(xdata, ydata):
     # number of dimensions of input data:
     ndims = xdata.shape[1]
     #print("ndims", ndims)
-    # number of points in each dimension:
-    npoints = nrowsx
 
     # initialize vector of complexity values (one value per dimension):
     complexityvalues = np.zeros(ndims)
 
-    # sort the x values on each dimension (by ascending order):
-    index = xdata[:, 0].argsort()
-    sortedxdata = xdata[index]
-
-    #print("sortedxdata: ", sortedxdata)
-    #print("index: ",  index)
-    # sort the y values (using the ndims different orders):
-    ydata = np.kron(np.ones((1, ndims)), ydata)
-    sortedydata = ydata[index]
-    print("sortedydata", sortedydata)
-
     # calculate complexity values for each dimension separately:
     for d in range(0, ndims):
+
+        xdata_d = xdata[:, d]
+        #print("checking dimension ", d, "with data ", xdata_d)
+
+        # sort the x values on each dimension (by ascending order), keeping the order index:
+        index = xdata_d.argsort()
+        sortedxdata = xdata_d[index]
+
+        #print("sortedxdata: ", sortedxdata)
+        #print("index: ",  index)
+
+        # sort the y values (using the the index):
+        sortedydata = ydata[index]
+        #print("sortedydata", sortedydata)
+
+
         # because the same x value may have different y values (when ndims>1)
         # we use the median of the y values. to do this:
 
-        # find the indexes with repeated x values:
-        x = sortedxdata[:, d]
-        print("checking dimension ",d, "with data ",x)
-        ux, i = np.unique(x, return_index=True)  # i returns the indexes of the unique values
-        print("ux ", ux, "i ", i)
-        ri = np.setdiff1d(range(0, npoints), i)  # ri returns the indexes of the repetitions
-        # (when there is a repeated value, i contains the index of the *first*
-        #  occurrence of the value, while ri contains the indexes of the n
-        #  first occurrences of the value (n>=1)
-        print("ri", ri)
-        if ri:  # if there are no repetitions skip this entire part
+        # find the indexes with repeated x values and create unique values array:
+        xfinal, indeces = np.unique(sortedxdata, return_index=True)  # indeces returns the indexes of the unique values
+        #print("xfinal ", xfinal, "indeces ", indeces)
+        # (when there is a repeated value, indeces contains the index of the *first*
+        #  occurrence of the value
 
-            # count occurrences, to help identify the "groups" of repetitions:
-            from collections import Counter
-            c = [Counter(x).values()]
-            # ans, c = countfind(x,ux)  # how many times each value appears
-            # c = c(c != 1)  # we are only interested if it appears more than once
-            c = [c[j] for j in range(len(c)) if c[j] != 1]
-            # (c now contains the number of occurrences of each repeated value)
-            # (c and ri can now be used to transform the y vector to replace
-            #  repetitions with median values)
+        if len(xfinal) != len(sortedxdata):  # if there are no repetitions skip this entire part
+            yfinal = np.zeros(len(xfinal))
+            for i in range(0,len(indeces)):
 
-            # build final y vector with medians where necessary:
-            y = sortedydata[:, d]
-            yfinal = np.zeros(len(y))
-            endlastrep = 0  # index on the y vector, end of last repetition
-            endthisrep = 0
-            for i in range(0, len(c)-1):
-                beginthisrep = ri[sum(c[1:i - 1])-(i - 1) + 1]
-                yfinal = [[yfinal],[y[endlastrep + 1: beginthisrep - 1]]]
-                # (appends to yfinal everything until this repetition)
-                endthisrep = beginthisrep + c[i] - 1
-                # (take note where this repitition ends)
-                yfinal[len(yfinal) + 1 , 1] = np.median(y[beginthisrep:endthisrep])
-                # (appends to yfinal the median value)
-                # (do NOT remove the ,1 or you may get a row instead of column)
-                endlastrep = endthisrep
+                # special case: repetition at the end of the sortedxdata
+                if i == len(indeces)-1:
+                    next_index = len(sortedydata)
+                else:
+                    next_index = indeces[i+1]
 
-            yfinal = [[yfinal],[y[endthisrep + 1: len(y)]]]
-            # (appends to yfinal everything after the final repetition)
+               # from x_i to x_i+1 there are repetions
+                yfinal[i] = np.median(sortedydata[indeces[i]:next_index])
+                #print("yfinal[",i,"]:", yfinal[i])
 
-            # also build final x vector:
-            xfinal = ux  # no secret here, just take the unique values
         else:
-            yfinal = sortedydata[:, d]
-            xfinal = x
+            yfinal = sortedydata[:, 0]
 
-        # if ~isempty(ri)
+        #print("yfinal", yfinal)
 
         # finally, calculate slopes using xfinal and yfinal:
         xfinal1 = xfinal[0:len(xfinal) - 1]
@@ -110,7 +89,7 @@ def complexity(xdata, ydata):
         # complexity of this dimension is the sum of the differences:
         cvalue = sum(slopes0)
         complexityvalues[d] = cvalue
-        print("complexity in dimension", d, ": ", complexityvalues)
+        print("complexity in dimension", d, ": ", complexityvalues[d])
 
 
     # - Now we use whatever we want from this vector
@@ -120,23 +99,23 @@ def complexity(xdata, ydata):
 # sum elements in vector!
 
 
-#X_test = np.array([
-#    [5],
-#    [2],
-#    [9],
-#    [0],
-#    [7],
-#    [7],
-#])
-#y_test = np.array([
-#    [1],
-#    [5],
-#    [7],
-#    [8],
-#    [4],
-#    [0],
-#     ])
-#complexityarray = complexity(X_test, y_test)
+X_test = np.array([
+    [5],
+    [2],
+    [9],
+    [0],
+    [7],
+    [7],
+])
+y_test = np.array([
+    [1],
+    [5],
+    [7],
+    [8],
+    [4],
+    [0],
+     ])
+complexityarray = complexity(X_test, y_test)
 
 X_test = np.array([
 [0],
@@ -156,18 +135,47 @@ complexityarray = complexity(X_test, y_test)
 
 
 
-# X_test = np.array([
-#     [0],
-#     [1],
-#     [2],
-# [3],
-# [4],
-# ])
-# y_test = np.array([
-#     [0],
-#     [1],
-#     [2],
-#     [1],
-#     [2],
-# ])
-# complexityarray = complexity(X_test, y_test)
+X_test = np.array([
+    [0],
+    [1],
+    [2],
+    [3],
+    [4],
+ ])
+y_test = np.array([
+    [0],
+    [1],
+    [2],
+    [1],
+    [2],
+ ])
+complexityarray = complexity(X_test, y_test)
+
+X_test = np.array([
+    [0, 0],
+    [1, 1],
+    [2, 2],
+    [3, 3],
+    [4, 4],
+ ])
+y_test = np.array([
+    [0],
+    [1],
+    [2],
+    [1],
+    [2],
+ ])
+complexityarray = complexity(X_test, y_test)
+
+
+X_test = np.array([
+    [0, 5, 9],
+    [1, 2, 1],
+    [2, 8, 0],
+ ])
+y_test = np.array([
+    [0],
+    [1],
+    [2],
+ ])
+complexityarray = complexity(X_test, y_test)
